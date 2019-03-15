a18967824ce64462e60648b434b7e806dbb030eb
Alejandro Zaizar <alexzaizar09@gmail.com>
2019-03-14 21:54:34 -0600

    media aritmetica

---
+++ main.rb
@@ -0,0 +1,5 @@
+require './medias'
+
+arr_nums = [1,2,3,4,5]
+
+p Medias.media_aritmetica(arr_nums)
---
+++ medias.rb
@@ -0,0 +1,19 @@
+﻿module Medias
+
+    # Calcula y regresa la media artimética
+    # public static double mediaAritmetica(params int[] vals) { }
+    def self.media_aritmetica(arr_nums)
+        arr_nums.reduce(:+).to_f/arr_nums.length.to_f
+    end
+    
+    # Calcula y regresa la raíz enésima = x^(1/n)
+    # private static double raizEnesima(double x, int n) { }
+
+    
+    # Usa raizEnesima para calcular y regresar la media geométrica
+    # public double mediaGeometrica(params int[] vals) { }
+
+    
+    # Este método no está implementado.
+    # public static double mediaArmonica(params int[] vals) { }
+end
