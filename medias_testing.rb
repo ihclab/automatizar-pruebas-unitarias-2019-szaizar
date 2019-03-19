6245c89a4978edba265012c661d84dc9e53c10e6
Alejandro Zaizar <alexzaizar09@gmail.com>
2019-03-19 15:17:49 -0600

    problema3 => procesamiento linea por linea

--- medias_testing.rb
+++ medias_testing.rb
@@ -1,5 +1,6 @@
 File.open("./CasosPrueba.txt") do |file|
     file.readlines.each do |line|
-        p line.chomp
+        id, fn, values, result = line.chomp.split(':')
+        p id, fn, values.chomp.split(",").map{|v| v.to_f}, result.to_f
     end
 end
