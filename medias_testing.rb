5e26e925cabb8e53158e7a4ec8847fadf89d697e
Alejandro Zaizar <alexzaizar09@gmail.com>
2019-03-24 16:43:00 -0600

    problema5 => determinando exito o fallo

--- medias_testing.rb
+++ medias_testing.rb
@@ -1,6 +1,24 @@
+require "./medias"
+
 File.open("./CasosPrueba.txt") do |file|
     file.readlines.each do |line|
-        id, fn, values, result = line.chomp.split(':')
-        p id, fn, values.chomp.split(",").map{|v| v.to_f}, result.to_f
+        # exctracting each value from each line in the file
+        id, fn, values, expected = line.chomp.split(':')
+
+        # converting the values to be sent into the function into the correct data type
+        values = values.chomp.split(",").map{|v| if v == "nil" then nil else v.to_f end}.compact
+        
+        expected = if expected == "nil" then nil else expected.to_f end
+        
+        # calling the corresponding method with an error handling exception
+        begin
+            obtained = Medias.send("media_#{fn}", values)
+        rescue
+            puts "\nEl metodo para calcular la media #{fn} no ha sido implementado"
+            puts "\n"
+        end
+
+        # Logging data to the console
+        puts "Id de prueba:  #{id} tipo de media: #{fn} prueba: #{if obtained == expected then "exitosa" else "fallida" end}"
     end
 end
